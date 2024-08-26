1a1da6f6765507ded4d864e61482f60546987703
Rexzinhoo <viniciussorgetzalves@gmail.com>
2024-08-26 10:37:26 -0300

    Ajuste na origin do socket

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: "https://prismatic-crostata-25a9f0.netlify.app",
       }
 });
 
